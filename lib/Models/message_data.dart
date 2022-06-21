a517fe8cb2d9b4770d782479240336597004093c
wilbert <wilberthy11@gmail.com>
2022-06-21 20:14:38 +0700

    adding package and add export excel functionality

--- lib/Models/message_data.dart
+++ lib/Models/message_data.dart
@@ -4,19 +4,30 @@ import 'package:firebase_database/firebase_database.dart';
 class Data {
   String? message;
   int? label;
+  int? correction;
   DateTime? created_at;
 
   Data(this.message, this.label);
 
   Map<String, dynamic> toMap() {
-    return {'message': message, 'label': label, 'created_at': created_at};
+    return {
+      'message': message,
+      'label': label,
+      'correction': correction,
+      'created_at': created_at
+    };
   }
 
-  Data.fromSnapshot(snapshot):
-      message = snapshot.data()['message'],
-      label = snapshot.data()['label'],
-      created_at = snapshot.data()['created_at'].toDate();
+  Data.fromSnapshot(snapshot)
+      : message = snapshot.data()['message'],
+        label = snapshot.data()['label'],
+        correction = snapshot.data()['correction'],
+        created_at = snapshot.data()['created_at'].toDate();
 
-  Map<String, dynamic> toJson() =>
-      {'message': message, 'label': label, 'created_at': created_at};
+  Map<String, dynamic> toJson() => {
+        'message': message,
+        'label': label,
+        'correction': correction,
+        'created_at': created_at
+      };
 }
